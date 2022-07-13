7c53aea7d1591f400ffcb9fd61a9279dcbdca2fd
Dipak <dipak.dhondge@nttdata.com>
2022-07-13 17:25:18 +0530

    Commit and Push to Test branch

--- TestData/ViewController.swift
+++ TestData/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var numbers: [Int] = [0,2,1,3,6,4,9,7,8]
+        _ = numbers.sorted()
+        var ascendingNumbers = numbers.sorted { (a, b) -> Bool in
+            a>b
+        }
+
+        //closure syntax
+        ascendingNumbers = numbers.sorted(by: >)
+
+        print(numbers.sort())
+
+        print(numbers)
+        print(ascendingNumbers)
     }
 
 
